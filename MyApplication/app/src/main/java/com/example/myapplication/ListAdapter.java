7a7e39a0418f23d4efc02fcab2278e3d5ed2c670
francisco <franter@live.cl>
2022-09-08 16:21:29 -0300

    Recycler

---
+++ MyApplication/app/src/main/java/com/example/myapplication/ListAdapter.java
@@ -0,0 +1,69 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
+
+    private List<ListElement> mData;
+    private LayoutInflater mInflater;
+    private Context context;
+
+
+    public ListAdapter(List<ListElement> itemList, Context context){
+        this.mInflater = LayoutInflater.from(context);
+        this.context = context;
+        this.mData = itemList;
+    }
+
+    @Override
+    public int getItemCount(){
+        return mData.size();
+    }
+
+    @Override
+    public ListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View view = mInflater.inflate(R.layout.list_element, null);
+        return new ListAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ListAdapter.ViewHolder holder, final int position){
+        holder.bindData(mData.get(position));
+    }
+
+    public void setItems(List<ListElement> items){mData = items;}
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView iconImage;
+        TextView country, city, estatus;
+
+        ViewHolder(View itemView){
+            super(itemView);
+            iconImage = itemView.findViewById(R.id.iconImageView);
+            country = itemView.findViewById(R.id.countryTextView);
+            city = itemView.findViewById(R.id.cityTextView);
+            estatus = itemView.findViewById(R.id.statusTextView);
+        }
+
+        void bindData(final ListElement item){
+            iconImage.setColorFilter(Color.parseColor(item.getColor()), PorterDuff.Mode.SRC_IN);
+            country.setText(item.getCountry());
+            city.setText(item.getCity());
+            estatus.setText(item.getEstatus());
+        }
+    }
+
+
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<ListElement> elements;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    public void init(){
+        elements = new ArrayList<>();
+        elements.add(new ListElement("#775447","Chile","Santiago","Activo"));
+        elements.add(new ListElement("#775447","Mexico","Mexico DF","Activo"));
+        elements.add(new ListElement("#775447","Argentina","Buenos Aires","Activo"));
+        elements.add(new ListElement("#775447","Peru","Lima","Activo"));
+        elements.add(new ListElement("#775447","Uruguay","Montevideo","Activo"));
+
+        ListAdapter listAdapter = new ListAdapter(elements, this);
+        RecyclerView recyclerView = findViewById(R.id.listRecycle);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(listAdapter);
+    }
+}
